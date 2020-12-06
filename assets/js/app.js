a8e9a990da2f3345da740cc60449bdea180c6c8a
SheikhRashed <mdrashed1310@gmail.com>
2020-12-06 19:00:42 +0600

    modify prevent

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,27 @@ $('.toggle').on('click', function () {
 });
 
 // prevent  inspect
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-});
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    console.log('You cannot inspect Element');
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    console.log('You cannot inspect Element');
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    console.log('You cannot inspect Element');
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    console.log('You cannot inspect Element');
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    console.log('You cannot inspect Element');
+    return false;
+  }
+};
+// prevents right clicking
+document.addEventListener('contextmenu', (e) => e.preventDefault());
