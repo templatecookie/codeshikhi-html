250fbf3bf3ccfef3bbed446ca38906a7b74eb425
Zakir Hossen <web.zakirbd@gmail.com>
2020-12-08 17:31:39 +0600

    responsive fixing on homepage and course details page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,27 +4,27 @@ $('.toggle').on('click', function () {
 });
 
 // prevent  inspect
-document.onkeydown = function (e) {
-  if (e.keyCode == 123) {
-    console.log('You cannot inspect Element');
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    console.log('You cannot inspect Element');
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-    console.log('You cannot inspect Element');
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    console.log('You cannot inspect Element');
-    return false;
-  }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    console.log('You cannot inspect Element');
-    return false;
-  }
-};
+// document.onkeydown = function (e) {
+//   if (e.keyCode == 123) {
+//     console.log('You cannot inspect Element');
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//     console.log('You cannot inspect Element');
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//     console.log('You cannot inspect Element');
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//     console.log('You cannot inspect Element');
+//     return false;
+//   }
+//   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//     console.log('You cannot inspect Element');
+//     return false;
+//   }
+// };
 // prevents right clicking
-document.addEventListener('contextmenu', (e) => e.preventDefault());
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
